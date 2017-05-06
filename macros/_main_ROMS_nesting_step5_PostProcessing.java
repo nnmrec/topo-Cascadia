fbb3fe35f5e69ba919973c186a529c841a52873d
Danny Clay Sale <dsale@uw.edu>
2017-05-06 02:56:21 -0700

    add post processing macro to export all reports and scenes

---
+++ macros/_main_ROMS_nesting_step5_PostProcessing.java
@@ -0,0 +1,40 @@
+// STAR-CCM+ macro: _main_ROMS_nesting_step5_PostProcessing.java
+// Written by STAR-CCM+ 10.06.010
+// 
+// license: ?
+// 
+
+package macro;
+
+import java.util.*;
+import star.common.*;
+
+///////////////////////////////////////////////////////////////////////////////
+// This is the MAIN driver, which calls all the other macros
+//
+public class _main_ROMS_nesting_step5_PostProcessing extends StarMacro {
+
+
+  public void execute() {
+    execute0();
+  }
+
+  private void execute0() {
+    // 
+    // as of Feb 14, 2017, try this:
+    // 
+    Simulation simulation_0 = getActiveSimulation();
+
+    // 
+    // POST-PROCESSING
+    // 
+    // export important data here    
+    new StarScript(getActiveSimulation(), new java.io.File(resolvePath("export_PointProbes.java"))).play(); 
+    new StarScript(getActiveSimulation(), new java.io.File(resolvePath("export_VirtualDisks.java"))).play(); 
+    new StarScript(getActiveSimulation(), new java.io.File(resolvePath("scene_SaveHardcopies.java"))).play();
+    // new StarScript(getActiveSimulation(), new java.io.File(resolvePath("save_ClearedMeshAndSolution.java"))).play();
+
+    simulation_0.saveState(getSimulation().getPresentationName()+".sim");
+
+  }
+}
---
+++ submit-post-processing-all-times.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+## --------------------------------------------------------
+
+## RENAME for your job
+#PBS -N topo-post-process-all-times
+
+## DIRECTORY where this job is run
+#PBS -d ./
+
+## GROUP to run under, or run under backfill
+## PBS -W group_list=hyak-stf
+#PBS -W group_list=hyak-motley
+
+## NUMBER nodes, CPUs per node, and MEMORY
+#PBS -l nodes=1:ppn=16,mem=180gb
+
+## WALLTIME (defaults to 1 hour, always specify for longer jobs)
+#PBS -l walltime=01:00:00
+
+## LOG the (stderr and stdout) job output in the directory
+#PBS -j oe -o .
+
+## EMAIL to send when job is aborted, begins, and terminates
+#PBS -m abe -M sale.danny@gmail.com
+
+## LOAD modules needed
+module load matlab_2015b
+module load contrib/starccm_12.02.010
+
+## RUN my simulation file in batch mode
+starSimFile="topo-Cascadia-ROMS-nesting"
+# ROMStime="1812"
+
+# matlab -nodesktop -nosplash < topoCascadia_UserInputs_time${ROMStime}.m 2>&1 | tee log.topoCascadia-time${ROMStime}
+cd cases/${starSimFile}
+echo 'running starccm from directory:'
+pwd
+starccm+ -batch ../../macros/_main_ROMS_nesting_step5_PostProcessing.java -np ${PBS_NP} -machinefile ${PBS_NODEFILE} -licpath 1999@mgmt2.hyak.local -batch-report ${starSimFile}.sim 2>&1 | tee log.post_${starSimFile}
+# cd ../..
+# mv cases/${starSimFile} cases/${starSimFile}_time${ROMStime}
+
+echo 'all finished, have a nice day'
